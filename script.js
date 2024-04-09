45c11dfa2486fe263970240473d994a80ef0319b
nyranrzz <ramazanlinuran@gmail.com>
2024-04-09 15:56:50 +0400

    updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let btn = document.querySelector('button');
 let upIcon = document.querySelector('i.up');
 let downIcon = document.querySelector('i.down');
@@ -7,7 +8,7 @@ let x = document.querySelector('.x');
 let input = document.querySelector('input');
 let span = document.querySelector('span.validation-span');
 let ol = document.querySelector('ol');
-
+let filter =document.querySelector('.filter');
 let tasks = [];
 let count = 0;
 
@@ -16,9 +17,9 @@ const deleteElement = (id) => {
     renderList();
 }
 
-const inputListChanger = () => {
+const inputListChanger = (e) => {
     
-    // e.preventDefault();
+    e.preventDefault();
     if ( input.value.trim().length  ) {
         tasks.push(input.value.trim());
         renderList();
@@ -27,7 +28,10 @@ const inputListChanger = () => {
         input.value = '';
         span.style.display = 'block';
     }
-
+    new Sortable(ol,{
+        animation : 500,
+        ghostClass : "sortable-ghost"
+    })
     inputPart.classList.toggle('d-none');
     listPart.classList.remove('d-none');
 }
@@ -41,7 +45,7 @@ const renderList = () => {
             <li>
                 <span class='task-inner'>${index+1}.${ task}</span>
              
-              <div class='icon'>  <i class="fa-regular fa-circle-xmark delete-btn" onclick="deleteElement(${index})"></i></div>
+              <div class='icon'><i class="fa-regular fa-circle-xmark delete-btn" onclick="deleteElement(${index})"></i></div>
             </li>`;
     });
 }
@@ -49,18 +53,33 @@ const renderList = () => {
 const clearInputValue = () => {
     input.value = '';
 }
-
-const filterAZ = () => {
-    tasks.sort();
-    renderList();
-}
-
-const filterZA = () => {
-    tasks.sort((a, b) => b.localeCompare(a));
-    renderList();
-}
-
+let sortBtn= "sortUp";
+filter.addEventListener('click',()=>{
+    if(sortBtn==="sortUp"){
+        
+        filter.style.background='url(img/yuxari.png)';
+        tasks.sort();
+        renderList();
+        sortBtn= "sortDown";
+        filter.addEventListener('mouseout', (e)=>{
+            e.target.style.background = "url(img/yuxarinoqara.png)"
+        })
+        filter.addEventListener('mouseover', (e)=>{
+            e.target.style.background = "url(img/yuxari.png)"
+        })
+    }else{
+       sortBtn ="sortUp"
+        filter.style.background='url(img/asagi.png)';
+        tasks.sort((a, b) => b.localeCompare(a));
+        renderList();
+        
+        filter.addEventListener('mouseout', (e)=>{
+            e.target.style.background = "url(img/asaginoqara.png)"
+        })
+        filter.addEventListener('mouseover', (e)=>{
+            e.target.style.background = "url(img/asagi.png)"
+        })
+    }
+})
 btn.addEventListener('click', inputListChanger);
-upIcon.addEventListener('click', filterZA);
-downIcon.addEventListener('click', filterAZ);
 x.addEventListener('click', clearInputValue);
